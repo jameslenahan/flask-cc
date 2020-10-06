4c658d61ce24908220fcbb63cd39152834233152
jameslenahan <57712403+jameslenahan@users.noreply.github.com>
2020-10-06 19:34:34 -0400

    User & additions added

---
+++ CC_Flask/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280bjf345'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from CC_Flask import routes
+
+
+#backref is similar to adding another post to the model, Lazy defines when SQLALCHEMY loads data from database
---
+++ run.py
@@ -0,0 +1,4 @@
+from CC_Flask import app
+
+if __name__ == '__main__':
+    app.run(debug=True)
